7f102eb244a202d39ea59e81a0e009984442aa40
daniel natale <dnatale667@gmail.com>
2021-11-24 17:56:46 -0500

    bug fix

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 
 // //login route on homepage
-router.get('/login', (req, res) => {
+router.get('/', (req, res) => {
     res.render('login');
   });
 module.exports = router;
