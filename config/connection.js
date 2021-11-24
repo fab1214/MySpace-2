66188b55c89a3a8f3a05127128924c7142b800aa
daniel natale <dnatale667@gmail.com>
2021-11-24 15:34:56 -0500

    hide password

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const Sequelize = require ('sequelize');
 require('dotenv').config();
 
 //create connection to our database , pass in your MySQL information for username and password as parameters
-const sequelize = new Sequelize('myspace_2_db', 'root', 'Tw@tson100', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
@@ -13,3 +13,4 @@ module.exports = sequelize;
 
 
 // process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW
+// 'myspace_2_db', 'root', 'Tw@tson100'
