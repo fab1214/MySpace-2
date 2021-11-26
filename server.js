3cac3e805f69bab5e660a8a60fa6f1e175fd8399
daniel natale <dnatale667@gmail.com>
2021-11-26 13:02:49 -0500

    create page

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const sess = {
   })
 };
 
-app.use(session(sess));
+
 
 const hbs = exphbs.create({});
 
@@ -30,7 +30,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(session(sess));
 app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
