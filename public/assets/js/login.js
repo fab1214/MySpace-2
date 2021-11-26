951f1ab9769aabd4d47efdced598418ad59dc010
daniel natale <dnatale667@gmail.com>
2021-11-26 17:26:56 -0500

    login and user creation ready

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,34 +1,6 @@
 // const { response } = require("express");
 
-async function signupFormHandler(event) {
-  event.preventDefault();
 
-  const username = document.querySelector('#username-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
-  const first_name = document.querySelector('#first_name-signup').value.trim();
-  const last_name = document.querySelector('#last_name-signup').value.trim();
-
-  if (first_name && last_name && username && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'post',
-      body: JSON.stringify({
-        first_name,
-        last_name,
-        username,
-        email,
-        password
-      }),
-      headers: { 'Content-Type': 'application/json' }
-    });
-
-    if (response.ok) {
-      window.location.replace('http://github.com');
-    } else {
-      alert(response.statusText);
-    }
-  }
-}
 
 async function loginFormHandler(event) {
   event.preventDefault();
@@ -55,4 +27,4 @@ async function loginFormHandler(event) {
 }
 
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+
---
+++ public/assets/js/signup.js
@@ -0,0 +1,33 @@
+// const { response } = require("express");
+
+async function signupFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+  const first_name = document.querySelector('#first_name-signup').value.trim();
+  const last_name = document.querySelector('#last_name-signup').value.trim();
+
+  if (first_name && last_name && username && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'post',
+      body: JSON.stringify({
+        first_name,
+        last_name,
+        username,
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+      document.location.replace('/feed');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
