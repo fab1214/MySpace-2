19b90fcccd4e593b8ca0c8e1d6485db85dad3743
daniel natale <dnatale667@gmail.com>
2021-11-29 19:34:06 -0500

    fixe stuff

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -66,7 +66,8 @@ router.get('/feed', withAuth, (req, res) => {
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
 
-      res.render('feed', { posts })
+      res.render('feed', { posts, 
+        loggedIn: req.session.loggedIn })
     })
     .catch(err => {
       console.log(err);
